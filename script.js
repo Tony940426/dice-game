b3b7c9674aa3e13cfbacf9eea1b5f9d14dda4586
Tony Wang <tony94042@gmail.com>
2022-10-10 14:23:07 +1100

    Added dice roll feature, dice pic now showing

--- script.js
+++ script.js
@@ -4,8 +4,30 @@
 const scoreZero = document.getElementById('score--0');
 const scoreOne = document.getElementById('score--1');
 const diceEle = document.querySelector('.dice');
+const rollBtn = document.querySelector('.btn--roll');
+const holdBtn = document.querySelector('.hold--roll');
+const newBtn = document.querySelector('.new--roll');
+const currentZero = document.getElementById('current--0')
+const currentOne = document.getElementById('current--1')
 
 //starting conditons
+let currentScore = 0;
 scoreZero.textContent = 0;
 scoreOne.textContent = 0;
 diceEle.classList.add("hidden");
+
+//dice role
+rollBtn.addEventListener('click', () => {
+  const score = Math.trunc(Math.random() * 6) + 1;
+  diceEle.classList.remove("hidden")
+  diceEle.src = `dice-${score}.png`;
+  console.log(score);
+
+  if(score === 1) {
+    scoreZero.textContent = 0;
+  } else {
+    scoreZero.textContent = Number(scoreZero.textContent) + score
+    currentScore += score
+    currentZero.innerText = currentScore
+  }
+})
